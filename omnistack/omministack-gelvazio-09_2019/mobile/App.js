0b82f6ac38d25ddbc3cf5d75e974ffb0c146c1bb
Gelvazio Camargo <gelvazio@hotmail.com>
2020-01-16 19:50:56 -0200

    inicial

---
+++ omnistack/omministack-gelvazio-09_2019/mobile/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Routes from './src/routes';
+
+//import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
+/*
+const styles = StyleSheet.create({
+  container: {
+    display:"flex",         //Isso já é padrão no Mobile e não precisa declarar
+    flexDirection:"column", //Isso já é padrão no Mobile e não precisa declarar
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
+
+*/
---
+++ omnistack/omministack-gelvazio-09_2019/mobile/src/pages/Book.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+import { View } from 'react-native';
+
+export default function Book() {
+  return <View />
+}
